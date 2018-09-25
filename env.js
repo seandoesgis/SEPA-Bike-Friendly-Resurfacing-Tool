16469226c50f4e64860f99a1ddf21d83faa07ffb
seandoesgis <slawrence@dvrpc.org>
2018-09-25 13:27:16 -0400

    small change to apiurl

--- env.js
+++ env.js
@@ -58,7 +58,7 @@ Global functions:
 /*global testLoad, ActiveXObject */
 var
   //    the URL of the ArcGIS API for JavaScript, you can change it to point to your own API.
-  apiUrl = null,
+  apiUrl = 'https://js.arcgis.com/3.25',
 
   //weinreUrl: String
   //    weinre is a tool which can help debug the app on mobile devices.
@@ -104,7 +104,7 @@ var
 
   ////////uncomment the following line when downloading the app
 
-  //apiUrl = 'https://js.arcgis.com/3.25';
+apiUrl = 'https://js.arcgis.com/3.25';
 
   //////////////////////////////////////////////////////////////
   allCookies = getAllCookies();
